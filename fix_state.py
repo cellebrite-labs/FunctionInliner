e322feef1ad0fa7eca9030dd82a7557e301ba5b1
Tomer Harpaz <tomer.harpaz1@cellebrite.com>
2022-04-26 22:32:00 +0300

    fix_state: fixed some bugs and added some more possible fixes

--- fix_state.py
+++ fix_state.py
@@ -9,8 +9,10 @@ import functioninliner
 # disable unused addresses (mapped, but not under any segment -> will always be unknown)
 print("DISABLING UNUSED ADDRESSES")
 
+
 def has_segment(ea):
     return bool(sark.Segment(ea).name)
+
 
 ea = idaapi.next_unknown(0, idaapi.BADADDR)
 while ea != idaapi.BADADDR:
@@ -24,11 +26,13 @@ while ea != idaapi.BADADDR:
 # 2. undo patched BLs to missing clones
 print("FIXING CLONES METADATA")
 
+
 def get_original_bytes(start_ea, size):
     orig = bytearray()
     for ea in range(start_ea, start_ea + size):
         orig.append(idaapi.get_original_byte(ea))
     return bytes(orig)
+
 
 def revert_patched_BL(src_ea, original_bytes=None):
     if original_bytes is None:
@@ -42,12 +46,15 @@ def revert_patched_BL(src_ea, original_bytes=None):
     print(f"reverting patch @ {src_ea:#x}")
     idaapi.patch_bytes(src_ea, original_bytes)
 
+    idaapi.plan_and_wait(src_ea, src_ea + 4)
+
     try:
         src_func = sark.Function(src_ea)
         for chunk_ea in functioninliner.unreachable_function_chunks_eas(src_func):
             idaapi.remove_func_tail(src_func._func, chunk_ea)
     except sark.exceptions.SarkNoFunction:
         pass
+
 
 def is_valid_branch(src_ea, target_ea):
     if not idaapi.is_mapped(src_ea):
@@ -60,13 +67,18 @@ def is_valid_branch(src_ea, target_ea):
     except sark.exceptions.SarkNoInstruction:
         return False
 
+
 def fix_func_tail(src_ea, clone_ea):
     try:
         src_func = sark.Function(src_ea)
         clone_end_ea = sark.Segment(clone_ea).end_ea
-        idaapi.append_func_tail(src_func._func, clone_ea, clone_end_ea)
+        # we used to just call append_func_tail, but on some IDA version
+        # we started getting occasional internal errors on some of these
+        if src_func.ea not in functioninliner.function_chunk_parent_eas(clone_ea):
+            idaapi.append_func_tail(src_func._func, clone_ea, clone_end_ea)
     except sark.exceptions.SarkNoFunction:
         pass
+
 
 storage = functioninliner.ClonesStorage()
 patches = sark.data.get_patched_bytes()
@@ -75,21 +87,34 @@ for func_ea, clones in list(storage.items()):
     for src_ea, clone_info in list(clones.items()):
         valid_src = is_valid_branch(src_ea, clone_info.clone_ea)
 
-        if valid_src and idaapi.is_mapped(clone_info.clone_ea):
+        if idaapi.is_mapped(clone_info.clone_ea):
+            clone_seg_name = sark.Segment(clone_info.clone_ea).name
+            parts = functioninliner.ClonesStorage.parse_storage_key(clone_seg_name)
+            if parts:
+                valid_target = parts["src_ea"] == src_ea
+            else:
+                valid_target = False
+        else:
+            valid_target = False
+
+        if valid_src and valid_target:
             # make sure the clone is a proper func tail of its caller
             fix_func_tail(src_ea, clone_info.clone_ea)
             continue
 
-        print(f"deleting clone @ {clone_info.clone_ea:#x}")
-        idaapi.del_segm(clone_info.clone_ea, idaapi.SEGMOD_KILL)
+        if valid_target:
+            print(f"deleting clone @ {clone_info.clone_ea:#x}")
+            idaapi.del_segm(clone_info.clone_ea, idaapi.SEGMOD_KILL)
 
         if valid_src:
-            revert_patched_BL(src_ea, clone_info.original_bytes)
+            print(f"reverting patch @ {src_ea:#x}")
+            revert_patched_BL(src_ea, clone_info.orig_bytes)
 
         del clones[src_ea]
 
 # look for dangling clones
 print("REMOVING DANGLING CLONES")
+
 
 def find_clone_info(clone_ea):
     storage = functioninliner.ClonesStorage()
@@ -97,6 +122,7 @@ def find_clone_info(clone_ea):
         for src_ea, clone_info in clones.items():
             if clone_info.clone_ea == clone_ea:
                 return (func_ea, src_ea, clone_info)
+
 
 for seg in list(sark.segments()):
     if not seg.name.startswith("inlined_"):
@@ -119,13 +145,16 @@ for seg in list(sark.segments()):
 # look for dangling patched BLs
 print("UNDOING DANGLING PATCHED BLs")
 
+
 def patch_size(p):
     size_bits = max(p.original.bit_length(), p.patched.bit_length())
     return (size_bits + 7) // 8
 
+
 def revert_range(start_ea, size):
     for ea in range(start_ea, start_ea + size):
         idaapi.revert_byte(ea)
+
 
 storage = functioninliner.ClonesStorage()
 
